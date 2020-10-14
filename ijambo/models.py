ec4ea3e05f2218f7dce52d2e11a939dce4806973
ArtcalO97 <niyca14@gmail.com>
2020-10-14 14:39:27 +0200

    All things update

--- ijambo/models.py
+++ ijambo/models.py
@@ -1,25 +1,20 @@
 from django.db import models
+
 class Societe(models.Model):
 	nom_societe = models.CharField(max_length=30)
 	pays = models.CharField(max_length=15)
 
-<<<<<<< HEAD
-# Create your models here.
-<<<<<<< HEAD
-<<<<<<< HEAD
 class Music(models.Model):
 	Titre=models.TextField(max_length=200)
-=======
+
 	def __str__(self):
 		return f" {self.nom_societe} "
->>>>>>> origin/Marley
-=======
+
 class Eleve(models.Model):
     nom = models.CharField(max_length=20)
     prenom = models.CharField(max_length=25)
->>>>>>> origin/shwaib
-=======
+
 class Eleve(models.Model):
     nom = models.CharField(max_length=20)
     prenom = models.CharField(max_length=25)
->>>>>>> origin/shwaib
+
