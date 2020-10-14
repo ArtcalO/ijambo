66a92f73429260c68b0e6533477ad600a38ab02b
Marley97 <hermesmarleyngenda@gmail.com>
2020-10-14 13:55:33 +0200

    creation d'un model Album

--- ijambo/models.py
+++ ijambo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class Societe(models.Model):
+	nom_societe = models.CharField(max_length=30)
+	pays = models.CharField(max_length=15)
 
-# Create your models here.
+	def __str__(self):
+		return f" {self.nom_societe} "
