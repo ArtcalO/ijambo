690ebfb4fd51a39cbe50229ec137008e049a3344
l3b0n <ndabashinzes@gmail.com>
2020-10-17 19:40:49 +0200

    urls-commit

--- ijambo/urls.py
+++ ijambo/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='ijambo-index'),
+    path('register/', views.register, name='ijambo-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='log.html'), name='ijambo-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='index.html'), name='ijambo-logout'),
 ]
