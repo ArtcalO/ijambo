d208081715f6c11a88ac45b8cf09d885162f7c1b
l3b0n <ndabashinzes@gmail.com>
2020-10-17 19:38:37 +0200

    Views-commit

--- ijambo/views.py
+++ ijambo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from .forms import *
 # Create your views here.
 
 class HomeView(View):
@@ -8,3 +9,14 @@ class HomeView(View):
 
 	def get(self, request, *args, **kwargs):
 		return render(request, self.template_name, locals())
+
+def register(request):
+    	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid:
+			form.save()
+			username = form.cleaned_data.get('username')
+			return redirect('ijambo-index')
+	else:
+		form = UserRegisterForm()
+	return render(request, 'register.html', locals())
