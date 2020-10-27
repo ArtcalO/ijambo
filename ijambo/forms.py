55a78a635c9c64a18605aa8d9a5ab729dbc1efde
Marley97 <hermesmarleyngenda@gmail.com>
2020-10-27 15:39:57 +0100

    hfhfhgfv

--- ijambo/forms.py
+++ ijambo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import *
+from . models import *
 class AlbumForm(forms.ModelForm):
 
 	titre=forms.CharField(
--- ijambo/models.py
+++ ijambo/models.py
@@ -7,7 +7,7 @@ class Album(models.Model):
 
 	titre = models.CharField(max_length=20)
 	cover = models.ImageField(upload_to ="music/covers")
-	music = models.ForeignKey(Music,on_delete = models.CASCADE)
+	music = models.ForeignKey(Music, on_delete = models.CASCADE)
 
 	def __str__(self):
 		return f"Titre : {self.titre} Cover : {self.cover} Music : {self.music.chanteur}"
@@ -15,7 +15,7 @@ class Album(models.Model):
 class Chanson_du_mois(models.Model):
 
 	channel = models.CharField(max_length=20)
-	audio = models.ForeignKey(Music,on_delete = models.CASCADE)
+	audio = models.ForeignKey(Music, on_delete = models.CASCADE)
 
 	def __str__(self): 
 		return f"Channel : {self.channel} Titre : {self.audio.chanteur}"
